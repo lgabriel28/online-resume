9a1430241208865cbd63250c7493fab82c2efd7c
Luis Santiago <luis.santiagoayala@gmail.com>
2015-11-07 17:56:12 -0800

    feat: Content formatting
    Added bootstrap to include image resposiveness and centralized content to improve readability and overall aesthetics.

--- js/helper.js
+++ js/helper.js
@@ -1,17 +1,4 @@
-/*
 
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr/>';
 
@@ -23,8 +10,8 @@ var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
-var HTMLbioPic = '<img src="%data%" class="biopic">';
-var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+var HTMLbioPic = '<img src="%data%" class="biopic img-responsive">';
+var HTMLwelcomeMsg = '<span class="welcome-message flex-box">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
@@ -40,7 +27,7 @@ var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectImage = '<img class="img-responsive" src="%data%" height="350" width="400">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
@@ -59,9 +46,6 @@ var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
 
 
-/*
-The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
-*/
 $(document).ready(function() {
   $('button').click(function() {
     var iName = inName(bio.name) || function(){};
@@ -70,7 +54,7 @@ $(document).ready(function() {
 });
 
 /*
-The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
+The next few lines about clicks are for the Collecting Click Locations.
 */
 clickLocations = [];
 
@@ -81,7 +65,7 @@ function logClicks(x,y) {
       y: y
     }
   );
-  console.log('x location: ' + x + '; y location: ' + y);
+  //console.log('x location: ' + x + '; y location: ' + y);
 }
 
 $(document).click(function(loc) {
@@ -93,15 +77,14 @@ $(document).click(function(loc) {
 
 
 /*
-This is the fun part. Here's where we generate the custom Google Map for the website.
+Generate the custom Google Map for the website.
 See the documentation below for more details.
 https://developers.google.com/maps/documentation/javascript/reference
 */
-var map;    // declares a global map variable
-
+var map;
 
 /*
-Start here! initializeMap() is called when page is loaded.
+initializeMap() is called when page is loaded.
 */
 function initializeMap() {
 
@@ -153,10 +136,10 @@ function initializeMap() {
   function createMapMarker(placeData) {
 
     // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.lat();  // latitude from the place service
-    var lon = placeData.geometry.location.lng();  // longitude from the place service
-    var name = placeData.formatted_address;   // name of the place from the place service
-    var bounds = window.mapBounds;            // current boundaries of the map window
+    var lat = placeData.geometry.location.lat();
+    var lon = placeData.geometry.location.lng();
+    var name = placeData.formatted_address;
+    var bounds = window.mapBounds;
 
     // marker is an object with additional data about the pin for a single location
     var marker = new google.maps.Marker({
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,5 @@
 
+// Define all objects needed to hold the data that will be displayed on the page.
 var bio = {
 	name: "Luis Santiago",
 	role: "Web Application Engineer",
@@ -9,9 +10,11 @@ var bio = {
 		location: "San Francisco"
 	},
 	picture:"images/profilePic.jpg",
-	welcome: "Welcome to my online resume! Take a look around, learn a little more about me, and feel free to connect.",
-	skills: ["Web development","Java","C#", "PHP", "HTML", "JavaScript", "CSS"]
+	welcome: "Welcome to my online resume! I am passionate about the fusion of creativity and science through the "+
+			 "creation of beautiful and robust web applications. Take a look around, learn a little more about me, and feel free to connect.",
+	skills: ["Web application development","C#", "PHP", "Java", "HTML", "JavaScript", "CSS"]
 };
+
 
 var work = {
 	"jobs":[
@@ -27,7 +30,17 @@ var work = {
 			"employer": "Premier Inc.",
 			"dates": "2010 - 2013",
 			"location": "Fort Lauderdale, FL, US",
-			"description": "Designed and developed new specialty disease treatment web applications, generating a revenue growth of approximately $10,000 a month. Developed web applications and Oracle 11g database table design to expedite productivity and reduce 30% of business processing time spent in the Sales and Marketing departments."
+			"description": "Designed and developed new specialty disease treatment web applications, generating a revenue growth " +
+						   "of approximately $10,000 per month. Developed web applications and Oracle 11g database table design to " +
+						   "expedite productivity and reduce 30% of business processing time spent in the Sales and Marketing departments."
+		},
+		{
+			"title": "Software Engineer",
+			"employer": "NASA Langley Research Center",
+			"dates": "2008-2009",
+			"location": "Hampton, VA, US",
+			"description": "Developed educational video game using bio-cybernetic technology for ADHD patients. Researched the use of brainwave " +
+						   "biofeedback in game-based learning environments to improve teaching and learning techniques."
 		}
 	]
 };
@@ -37,12 +50,7 @@ var projects = {
 		{
 			"title": "Portfolio Mockup to HTML",
 			"description": "Developed a personal portfolio page using HTML, CSS, and the Bootstrap framework. The page is fully responsive and works on mobile, tablet, and desktop browsers.",
-			"images": ["url1", "url2"]
-		},
-		{
-			"title": "Portfolio Mockup to HTML",
-			"description": "Developed a personal portfolio page using HTML, CSS, and the Bootstrap framework. The page is fully responsive and works on mobile, tablet, and desktop browsers.",
-			"images": ["url1", "url2"]
+			"images": ["images/project1.png"]
 		}
 	]};
 
@@ -52,13 +60,15 @@ var education = {
 	    	"name": "Interamerican University",
 	    	"location": "San Juan, PR, US",
 	    	"degree": "BA",
-	    	"major": "Comp Sci"
+	    	"major": "Computer Science",
+	    	"dates": "2004-2009 -- BS"
 	    },
 	    {
 	    	"name": "Nova Southeastern University",
 	    	"location": "Fort Lauderdale, FL, US",
 	    	"degree": "MS",
-	    	"major": "Management Information Systems"
+	    	"major": "Management Information Systems",
+	    	"dates": "2012-2015 -- MS"
 	    }
 ]};
 
@@ -66,23 +76,51 @@ var education = {
 var formattedName = HTMLheaderName.replace("%data%", bio.name) ;
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 var formattedBioPic = HTMLbioPic.replace("%data%", bio.picture);
-var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-var formattedEmail =  HTMLemail.replace("%data%", bio.contacts.email);
-var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
-var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
 var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcome);
-var formattedContact = HTMLcontactGeneric.replace("%data%", "DATA");
-
 $("#header").append(formattedName);
 $("#header").append(formattedRole);
-$("#header").append(formattedWelcome);
 $("#header").append(formattedBioPic);
-$("#topContacts").append(formattedMobile);
-$("#topContacts").append(formattedEmail);
-$("#topContacts").append(formattedGitHub.replace("%url%", bio.contacts.github));
-$("#topContacts").append(formattedLocation);
+$("#header").append(formattedWelcome);
 
-bio.displaySkills = function(skills){
+/**
+* @description Display contacts on either top or footer sections depending on the arguments beign passed.
+* @param {object} contacts - An object containing contact information.
+* @param {string} section - The section where to display contact information.
+*/
+bio.displayContacts = function(contacts, section){
+	// Initialize posible sections
+	var sectionId = ["#topContacts", "#footerContacts"];
+	var sections = ["top", "footer"];
+	// Check if section is valid
+	if(sections.indexOf(section) === -1){
+		return null;
+	}
+
+	// Assign section
+	if(section === "top"){
+		section = sectionId[0];
+	}
+	else{
+		section = sectionId[1];
+	}
+
+	var formattedMobile = HTMLmobile.replace("%data%", contacts.mobile);
+	var formattedEmail =  HTMLemail.replace("%data%", contacts.email);
+	var formattedGitHub = HTMLgithub.replace("%data%", contacts.github);
+	var formattedLocation = HTMLlocation.replace("%data%", contacts.location);
+
+	$(section).append(formattedMobile);
+	$(section).append(formattedEmail);
+	$(section).append(formattedGitHub.replace("%url%", contacts.github));
+	$(section).append(formattedLocation);
+
+}
+
+/**
+* @description Display set of skills added within the bio object.
+* @param {object} skills - An object containing professional skills.
+*/
+bio.display = function(skills){
 	if(skills.length > 0){
 	$("#header").append(HTMLskillsStart);
 		for(var i = 0; i < bio.skills.length; i++){
@@ -92,10 +130,11 @@ bio.displaySkills = function(skills){
 	}
 }
 
-
-work.displayWork = function(){
+/**
+* @description Display work experience.
+*/
+work.display = function(){
 	if(work.jobs.length > 0){
-
 		for(key in work.jobs){
 			$("#workExperience").append(HTMLworkStart);
 			var formattedemployer = HTMLworkEmployer.replace("%data%", work.jobs[key].employer);
@@ -111,6 +150,9 @@ work.displayWork = function(){
 	}
 }
 
+/**
+* @description Display projects and images.
+*/
 projects.display = function(){
 
 	for(key in projects.projects){
@@ -121,9 +163,44 @@ projects.display = function(){
 
 		$(".project-entry:last").append(formattedTitle);
 		$(".project-entry:last").append(formattedDescription);
+
+		if(projects.projects[key].images.length > 0){
+			projects.projects[key].images.forEach(function(val){
+				var formattedImage = HTMLprojectImage.replace("%data%", val);
+				$(".project-entry:last").append(formattedImage);
+			});
+		}
 	}
 }
 
+
+/**
+* @description Display set of skills added within the bio object.
+* @param {object} skills - An object containing professional skills.
+*/
+education.display = function(){
+	if(education.schools.length > 0){
+
+		for(key in education.schools){
+			$("#education").append(HTMLschoolStart);
+			var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[key].name);
+			var formattedDates = HTMLschoolDates.replace("%data%", education.schools[key].dates);
+			var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[key].location);
+			var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[key].major);
+
+			$(".education-entry:last").append(formattedSchoolName);
+			$(".education-entry:last").append(formattedDates);
+			$(".education-entry:last").append(formattedLocation);
+			$(".education-entry:last").append(formattedMajor);
+		}
+	}
+}
+
+
+/**
+* @description Formats the full name into a international-friendly format.
+* @param {string} fullname - Candidate's full name.
+*/
 function inName(fullName){
     var names = [];
 
@@ -136,10 +213,13 @@ function inName(fullName){
 	return firstName +" "+ lastName;
 }
 
-work.displayWork();
+// Invoke display functions.
+work.display();
 projects.display();
-bio.displaySkills(bio.skills);
-
+bio.display(bio.skills);
+education.display();
+bio.displayContacts(bio.contacts, "top");
+bio.displayContacts(bio.contacts, "footer");
 
 // show map
 $("#mapDiv").append(googleMap);
